aa5bfe1c10ff9c3ddecce8947e437125f106991c
Habiba Mahmoud <habibaabouelyazeed@gmail.com>
2023-10-22 21:36:24 +0300

    Counter using Redux

--- src/components/Shop/About.jsx
+++ src/components/Shop/About.jsx
@@ -1,7 +1,30 @@
+import { useSelector } from "react-redux";
+
 const About = () => {
-    return(
-        <h2>Info page</h2>
-    )
+    const counterVal = useSelector((state) => state.counter.counter)
+  return (
+    <div className="container py-5">
+      <h2 className="pb-4">About</h2>
+      <div className="row align-items-center">
+        <div className="col-8">
+          <h4 className="pb-4">Info</h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis,
+            voluptas. Alias, accusantium eum esse eaque harum, neque eius
+            accusamus recusandae fugiat cum, ab quisquam similique ipsam? Fugiat
+            sapiente quos eum!
+          </p>
+
+          <p className="mt-4 fs-5">Donations: $<span className="fw-bold">{counterVal}</span></p>
+        </div>
+        <div className="col-4">
+            <figure className="">
+                <img src='./images/avatar.png' className="img-fluid rounded-circle" alt="avatar" />
+            </figure>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from '../../redux/slices/counterSlice'
+
 
 const Counter = () => {
-  const [count, setCount] = useState(10);
-    
-  const incCount = () => {
-    setCount(count + 1);
+  const counter = useSelector((state) => state.counter.counter)
+  const dispatch = useDispatch();
+  
+  const handleIncrement = () => {
+    dispatch(increment());
   }
 
-  const decCount = () =>{
-    setCount(count - 1);
+  const handleDecrement = () =>{
+    dispatch(decrement());
   }
+
 
   return (
     <div className="container py-5 text-center">
       <h2>Counter page</h2>
       <div className="w-50 mx-auto d-flex flex-column justify-content-center align-items-center">
-        <div className="my-4 fs-2">{count}</div>
+        <div className="my-4 fs-2">Dontaions: $<span className="fw-bold">{counter}</span></div>
         <div className="d-flex gap-5">
-          <button className="btn btn-dark" onClick={decCount}>-</button>
-          <button className="btn btn-dark" onClick={incCount}>+</button>
+          <button className="btn btn-dark" onClick={handleDecrement}>-</button>
+          <button className="btn btn-dark" onClick={handleIncrement}>+</button>
         </div>
       </div>
     </div>
